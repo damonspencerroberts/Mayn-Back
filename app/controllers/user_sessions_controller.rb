c5e4386d66d3a2adff23568b4df4ab2ca4286e7a
damonweekendr <damon@weekendr.eu>
2021-09-11 17:00:18 +0200

    add verification if someone with username or email exists

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,5 @@
 class UserSessionsController < ApplicationController
   def verify
-    user_params = params[:user]
     authentic_user = User.authenticate(user_params[:email], user_params[:password])
     message = if authentic_user.nil?
                 { status: 0, message: 'The information you have entered is incorrect, please try again!' }
@@ -8,5 +7,24 @@ class UserSessionsController < ApplicationController
                 { status: 1, message: 'You successfully logged in!' }
               end
     render json: message
+  end
+
+  def exists
+    username_exists = User.find_by(username: user_params[:username])
+    email_exists = User.find_by(email: user_params[:email])
+    message = if email_exists
+                { status: 1, message: 'Sorry, someone with that email already exists' }
+              elsif username_exists
+                { status: 0, message: 'Sorry, someone with that username already exists' }
+              else
+                { status: 2, message: 'Success no one with those credentials exists' }
+              end
+    render json: message
+  end
+
+  private
+
+  def user_params
+    params[:user]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,20 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: %w[show]
   end
-  
-  devise_for :users,
-  defaults: { format: :json },
-  path: '',
-  path_names: {
-    sign_in: 'api/login',
-    sign_out: 'api/logout',
-    registration: 'api/signup'
-  },
-  controllers: {
-    sessions: 'sessions',
-    registrations: 'registrations'
-  }
 
-  post '/api/user_sessions', to: "user_sessions#verify"
+  devise_for :users,
+             defaults: { format: :json },
+             path: '',
+             path_names: {
+               sign_in: 'api/login',
+               sign_out: 'api/logout',
+               registration: 'api/signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
+
+  post '/api/user_sessions', to: 'user_sessions#verify'
+  post '/api/user_exists', to: 'user_sessions#exists'
 end
